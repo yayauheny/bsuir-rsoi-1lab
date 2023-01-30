28342121ac65d091a90656d8426b4f0f004eea76
artrayme <artrayme@protonmail.com>
2023-01-30 21:45:54 +0300

    add application plugin for gradle

--- application/src/main/java/com/github/rsoi/Main.java
+++ application/src/main/java/com/github/rsoi/Main.java
@@ -1,9 +1,7 @@
 package com.github.rsoi;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public class Main {
 	public static void main(String[] args) {
+		System.out.println("Hello, students!");
 	}
 }
